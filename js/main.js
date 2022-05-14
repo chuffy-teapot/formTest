25eca99c2088ed65b56b23545d26f0d4c00b8f99
chuffy-teapot <cvjjgvxqpzm@gmail.com>
2022-05-14 15:22:50 +0300

    edit and check add

--- js/main.js
+++ js/main.js
@@ -3,50 +3,94 @@ let INP = document.querySelector(".input");
 let BTN = document.querySelector(".btn");
 let form = document.querySelector(".form")
 
+let array = [];
+
+console.log("start");
 
 function foo() {
-  if (!INP.value) return;
-
   let li = document.createElement("li");
   let div = document.createElement("div");
   let span = document.createElement("span");
-  let check = document.createElement("input");
-  let del = document.createElement("button");
+  let check = document.createElement("button")
+  let del = document.createElement("button")
+  let edit = document.createElement("button")
 
-  check.setAttribute("type", "checkbox")
-  del.setAttribute("onclick", "rem()")
 
+  if (INP.value == "") {
+    alert("вы ничего не ввели")
+    return
+  }
+
+  else if (list.childNodes.length >= 15) {
+    alert("слишком много значений");
+    return
+  }
 
   span.textContent = INP.value;
 
   li.className = "list__item";
   div.className = "list__box";
-  del.className = "delete__btn"
   span.className = "list__item-span";
-  check.className = "checkbox";
+  del.className = "delete__btn"
+  check.className = "check__btn";
+  edit.className = "edit__btn"
 
-
-  del.append("delete")
+  del.append("удалить")
+  check.append("сделано")
+  edit.append("изменить")
   div.append(check);
-  div.append(span);
+  div.append(edit);
+  div.append(del);
+  li.append(span);
   li.append(div);
-  li.append(del);
   list.prepend(li);
 
   INP.value = "";
 
-  if (list.childNodes.length !== 1) {
+  del.addEventListener("click", () => {
+    let con = confirm("вы уверены ?");
+    if (con) li.remove();
+  })
+
+  edit.addEventListener("click", () => {
+    if (li.style.color == "white") {
+      return;
+    }
+    else {
+      let newList = prompt("введите значение")
+
+      if (newList !== "" && newList !== null) {
+        span.textContent = newList;
+      }
+    }
+  })
+
+  check.addEventListener("click", () => {
+    if (li.style.color == "white") {
+      li.style.backgroundColor = "#01a1a157";
+      li.style.color = "black"
+      span.style.textDecoration = "none";
+      edit.style.opacity = "1";
+      edit.style.cursor = "pointer"
+    }
+    else {
+      li.style.backgroundColor = "#00800081";
+      li.style.color = "white"
+      span.style.textDecoration = "line-through"
+      edit.style.opacity = ".4";
+      edit.style.cursor = "default"
+    }
+  })
+
+  if (list.childNodes.length !== 0) {
     form.style.borderBottomLeftRadius = "0px";
     form.style.borderBottomRightRadius = "0px";
   } else {
     form.style.borderBottomLeftRadius = "10px";
     form.style.borderBottomRightRadius = "10px";
   }
-
 }
 
-const rem = () => {
-  let btn = document.querySelectorAll(".delete__btn");
 
-  console.log(btn.parentNode);
-}
+
+console.log("end");
